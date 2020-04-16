12adeeb0afcda922745a92fd4cd4e44ab9816156
Saulo Duarte <sauloduarte@MacBook-Pro-de-Saulo.local>
2020-04-16 16:21:01 -0300

    UP

---
+++ android/src/main/java/io/flutter/plugins/webviewflutter/WebViewFactory.java
@@ -0,0 +1,30 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.webviewflutter;
+
+import android.content.Context;
+import android.view.View;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.StandardMessageCodec;
+import io.flutter.plugin.platform.PlatformView;
+import io.flutter.plugin.platform.PlatformViewFactory;
+import java.util.Map;
+
+public final class WebViewFactory extends PlatformViewFactory {
+  private final PluginRegistry.Registrar registrar;
+
+  WebViewFactory(PluginRegistry.Registrar registrar) {
+    super(StandardMessageCodec.INSTANCE);
+    this.registrar = registrar;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public PlatformView create(Context context, int id, Object args) {
+    Map<String, Object> params = (Map<String, Object>) args;
+    return new FlutterWebView(context, id, params, registrar);
+  }
+}
